f8607facba22f3ef452ecbba8f8e3336c416a51f
Leonardo <matos_leo95@live.com>
2018-05-24 15:39:58 +0000

    Altera main para testes com Loja

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <memory>
-#include "produto.h"
-#include "fruta.h"
-#include "roupa.h"
-#include "bebida.h"
+#include "loja.h"
 
-using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	std::vector<shared_ptr<Produto>> lista;
+	Loja lista;
 	
-	lista.push_back(make_shared<Fruta>("001002003-45","Maca verde",8.70,"01/10/2017",18));
-	lista.push_back(make_shared<Fruta>("001002004-44","Laranja",4.75,"23/09/2017",15));
-	lista.push_back(make_shared<Fruta>("001002005-11","Limao verde",2.30,"01/10/2017",25));
-	lista.push_back(make_shared<Roupa>("001004011-56","Camisa Xadrez",56.70,"12/04/2017","G"));
-	lista.push_back(make_shared<Bebida>("001007025-32","Vinho do Porto",367.21,"30/08/1829",14.5));
+	lista.removeProduto("001002003-45");
+	lista.lerFrutas();
+	lista.lerBebidas();
+	lista.lerRoupas();
+	lista.listarProdutos();
+	lista.removeProduto("001002003-45");
+	lista.listarProdutos();
 
-	for (auto i = lista.begin(); i != lista.end(); ++i)
-	{
-		std::cout << (**i) << std::endl;	
-	}
+	
 
 	return 0;
 }
